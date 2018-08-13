0fb4bcd62e5c0b29f6bca9eccc0b8aeac76569bf
brandonjunus <brandonjunus2@gmail.com>
2018-08-13 14:30:21 -0700

    completed bare minimum requirements

--- app.js
+++ app.js
@@ -1,1 +1,48 @@
-// TODO
+class GroceryListItems extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      done: false
+    }
+  }
+  onListItemClick() {
+    this.setState({
+      done: !this.state.done
+    });
+  }
+  render() {
+  
+  var style = {
+    'font-weight': this.state.done ? 'bold' : 'normal'
+  };
+  
+    return (
+      <li style={style} onMouseEnter={this.onListItemClick.bind(this)} onMouseLeave={this.onListItemClick.bind(this)}>{this.props.todo}</li>
+    );
+  }
+}
+
+var GroceryList = (props) => (
+  <ul>
+    {props.todos.map(todo =>
+      <GroceryListItems todo={todo} />
+    )}
+  </ul>
+);
+
+var App = () => (
+  <div>
+    <h2>Grocery List</h2>
+    <GroceryList todos={["cucumber", "kale"]}/>
+  </div>
+);
+
+ReactDOM.render(<App />,document.getElementById('app'));
+
+
+
+
+
+ReactDOM.render(<App />,document.getElementById('app'));
+
+
